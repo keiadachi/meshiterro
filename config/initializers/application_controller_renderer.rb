f6119ddcc883c310ac9ca39d14ddae05299728e8
kei adachi <2013edcckeiadachi@gmail.com>
2021-03-10 16:31:59 +0000

    meshiterro

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5c80aa3721051d8f47980f604badcd501349d50469a3905edcca97579c28439dc2918d2185ef576877108e02ca300647033dee8de48827720acdf3da6b809a55'
